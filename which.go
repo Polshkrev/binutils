a1939c8c52d8d59a0866ceae40a55d9795fca5a9
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2025-01-02 15:07:15 -0500

    Add which single function

--- which.go
+++ which.go
@@ -8,15 +8,25 @@ import (
 	"github.com/Polshkrev/gopolutils/collections"
 )
 
+func which(programme string) (string, *gopolutils.Exception) {
+	var result string
+	var err error
+	result, err = exec.LookPath(programme)
+	if err != nil {
+		return "", gopolutils.NewException(fmt.Sprintf("executable file '%s' not found in system path.", programme))
+	}
+	return result, nil
+}
+
 func Which(programmes ...string) (collections.Collection[string], *gopolutils.Exception) {
 	var programme string
 	var results collections.Collection[string] = collections.NewArray[string]()
 	for _, programme = range programmes {
 		var result string
-		var err error
-		result, err = exec.LookPath(programme)
-		if err != nil {
-			return nil, gopolutils.NewException(fmt.Sprintf("executable file '%s' not found in system path.", programme))
+		var except *gopolutils.Exception
+		result, except = which(programme)
+		if except != nil {
+			return nil, except
 		}
 		results.Append(result)
 	}
