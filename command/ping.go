cb0c0572c5a753476592bc439f606a88788727b8
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2025-04-17 12:54:42 -0400

    Create ping.go

---
+++ command/ping.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Polshkrev/binutils"
+	"github.com/Polshkrev/gopolutils"
+	"github.com/Polshkrev/gopolutils/collections"
+)
+
+func main() {
+	if len(os.Args) < 1 {
+		fmt.Fprintln(os.Stderr, gopolutils.NewException("No URLs provided.").Error())
+		os.Exit(1)
+	}
+	var statusCodes collections.Collection[uint16] = gopolutils.Must(binutils.Ping(os.Args...))
+	var code uint16
+	for _, code = range statusCodes.Collect() {
+		fmt.Println(code)
+	}
+}
